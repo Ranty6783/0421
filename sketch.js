2ec385aac1971a98a221057e0485b4f90ce3685b
Ranty6783 <ranger6783@gmail.com>
2026-04-21 10:46:50 +0800

    1.3 馬賽克

--- sketch.js
+++ sketch.js
@@ -19,13 +19,30 @@ function draw() {
   let x = (width - w) / 2;
   let y = (height - h) / 2;
 
-  // 將攝影機畫面繪製到 pg 物件中
-  pg.image(cam, 0, 0, pg.width, pg.height);
+  // 產生馬賽克效果
+  let stepSize = 20; // 馬賽克方塊的大小，數字越大馬賽克感越強
+  cam.loadPixels();
+  
+  if (cam.pixels.length > 0) {
+    pg.clear(); // 清除上一幀的繪圖
+    pg.noStroke();
+    for (let cy = 0; cy < cam.height; cy += stepSize) {
+      for (let cx = 0; cx < cam.width; cx += stepSize) {
+        // 取得該像素點的顏色資訊 [r, g, b, a]
+        let offset = (cy * cam.width + cx) * 4;
+        let r = cam.pixels[offset];
+        let g = cam.pixels[offset + 1];
+        let b = cam.pixels[offset + 2];
+        
+        pg.fill(r, g, b);
+        pg.rect(cx, cy, stepSize, stepSize); // 在 pg 上繪製馬賽克方塊
+      }
+    }
+  }
 
   push(); // 儲存目前的座標狀態
   translate(x + w, y); // 將原點移至影像預計出現的右側
   scale(-1, 1); // 水平翻轉座標系
-  image(cam, 0, 0, w, h); // 在翻轉後的座標系繪製影像
   image(pg, 0, 0, w, h); // 將圖層繪製在視訊畫面上方（同樣會受 scale 影響而鏡像修正）
   pop(); // 恢復原始座標狀態，以免影響後續繪圖
 }
